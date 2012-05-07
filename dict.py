09eb717e650465fa596806b0fcae3284d60de6f3
cat <z82206.cat@gmail.com>
2012-05-07 21:50:08 +0800

    using yahdict

--- dict.py
+++ dict.py
@@ -1,99 +1,61 @@
 #!/usr/bin/env python 
 
-import urllib2
+import yahdict
 import sys
-import bs4
+
+def easy_print(l,name):
+    if l :
+        print "# "+name+" :"
+        for e in l :
+            print "    "+e["content"] #+"("+e["href"]+")"
+        print '' 
 
 if __name__ == "__main__":
-    url = 'http://tw.dictionary.yahoo.com/dictionary?p='+sys.argv[1].replace(" ","+")
-
-    html = urllib2.urlopen(url)
+    dic = yahdict.YahooDictAPI(sys.argv[1])
     
-    html_out = ""
-
-    for line in html:
-        html_out += line
-
-        if "type=text/javascript" in line:
-            break
-
-    soup = bs4.BeautifulSoup(html_out)
-    msg = soup.find(attrs={"class":"msg"})
-
-    if msg != None:
-        if "zrpmsg" in str(msg):
-            print "No found."
-            exit(0)
-            
-    summary = soup.find(attrs={"id":"summary-card"})
-    #print summary.prettify()
-   
+    if not dic.found():
+        print "[!] No found."
+        exit(0)
+    
     #summary-description
-    print sys.argv[1]+ " : "+summary.find("div","description").p.contents[0]
+    print sys.argv[1]+ " : "+dic.summary_desc
     print '' 
 
     #pronunciation
-    res = summary.find("div","pronunciation")
-    if res :
-        pron = str(res.div)
-        if "KK" in pron:
-            print "# Pronunciation :"
-            print "    KK : "+pron[pron.find("</span>")+8:pron.rfind("<span>")]
-            print "    DJ : "+pron[pron.rfind("</span>")+8:pron.rfind("</div>")]
-            print '' 
+    if dic.summary_pron:
+        print "# Pronunciation :"
+        print "    KK : "+dic.summary_pron["kk"]
+        print "    DJ : "+dic.summary_pron["dj"]
+        print '' 
 
     #related
-    res = summary.find(attrs={"class":"related"})
-    if res :
-        print "# Related :"
-        for data in res.find_all("li"):
-            print "    "+data.a.contents[0]
-        print '' 
+    easy_print(dic.summary_related,"Related")
 
     #Synonyms
-    res = summary.find(attrs={"class":"synonyms"})
-    if res :
-        print "# Synonyms :"
-        for data in res.find_all("a"):
-            print "    "+data.contents[0]
-        print '' 
+    easy_print(dic.summary_synonym,"Synonyms")
     
     #antonyms
-    res = summary.find(attrs={"class":"antonym"})
-    if res :
-        print "# Antonyms :"
-        for data in res.find_all("a"):
-            print "    "+data.contents[0]
-        print ''
+    easy_print(dic.summary_antonym,"Antonyms")
 
     #Variation
-    res = summary.find(attrs={"class":"variation"})
-    if res :
-        print "# Variation :"
+    easy_print(dic.summary_variation,"Variation")
+
+    #defition
+    for e in dic.defition :
+        if "caption" in e :
+            print "$ "+e["caption"]
+        
+        if "interpret" in e :
+            for inte in e["interpret"]:
+                print "   * "+inte[0]
+                
+                for exam in inte[1]:
+                    print "      >> "+exam[0]
+                    print "         "+exam[1]
+                print ''
+        print ''
+
+
         for data in res.find_all("a"):
             print "    "+data.contents[0]
         print '' 
-
-
-    defition = soup.find_all(attrs={"class":"def clr nobr"})
-    
-    for d in defition:
-        if d.find(attrs={"class":"caption"}).contents :
-            print "$ "+d.find(attrs={"class":"caption"}).contents[0]
-
-        for li in d.find_all("li"):
-            print "   * "+li.find("p","interpret").contents[0]
-
-            example =  li.find_all("p","example")
-            
-            for ex in example :
-                out = ""
-                for e in  ex.contents:
-                    out += unicode(e)
-                out = out.replace("<b>","").replace("</b>","")
-                outs = out.split("\n")
-                print "      >> "+outs[0]
-                print "         "+outs[1]
-            print ''
-        print ''
-
---
+++ yahdict.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python
+
+import urllib2
+import sys
+import bs4
+
+class YahooDictAPI:
+    
+    def __init__(self,word,space2add=True,url_prefix='http://tw.dictionary.yahoo.com/dictionary?p='):
+        self.request_url = url_prefix
+
+        if space2add :
+            self.request_url += word.replace(" ","+")
+        else :
+            self.request_url += word
+
+        html = urllib2.urlopen(self.request_url)
+        html_out = ""
+
+        for line in html:
+            html_out += line
+            
+            #find ending line
+            if "type=text/javascript" in line:
+                break
+
+        self.soup = bs4.BeautifulSoup(html_out)
+        self.msg = self.soup.find(attrs={"class":"msg"})
+        
+        if self.found() :
+            self.summary = self.soup.find(attrs={"id":"summary-card"})
+            self.defition = self.soup.find_all(attrs={"class":"def clr nobr"})
+            
+            self.summary_desc = self.summary.find("div","description").p.contents[0]
+            self.summary_pron = self.get_pron()
+            self.summary_related = self.get_related()
+            self.summary_synonym = self.get_synonym()
+            self.summary_antonym = self.get_antonym()
+            self.summary_variation = self.get_variation()
+            self.defition = self.get_defition()
+
+    def found(self):
+        return not "zrpmsg" in str(self.msg)
+
+    def get_pron(self):
+        res = self.summary.find("div","pronunciation")
+        if res :
+            d = dict()
+            pron = str(res.div)
+            if "KK" in pron:
+                d["kk"] = pron[pron.find("</span>")+8:pron.rfind("<span>")].strip()
+                d["dj"] = pron[pron.rfind("</span>")+8:pron.rfind("</div>")].strip()
+            return d
+        return None
+
+    def get_related(self):
+        return self.easy_fetch_li("related")
+
+    def get_synonym(self):
+        return self.easy_fetch_a("synonyms")
+
+    def get_antonym(self): 
+        return self.easy_fetch_a("antonym")
+
+    def get_variation(self):
+        return self.easy_fetch_a("variation")
+
+    def get_defition(self):
+        """
+            [
+                {
+                    "caption":caption,
+                    "interpret":interpret,
+                    "example":[(english,chinese),...]
+                },...
+            ]
+        """
+        defition = self.soup.find_all(attrs={"class":"def clr nobr"})
+        
+        out_list = list() 
+        for d in defition:
+            dd = dict()
+            if d.find(attrs={"class":"caption"}).contents :
+                dd["caption"] = d.find(attrs={"class":"caption"}).contents[0]
+                dd["interpret"] = list()
+
+            for li in d.find_all("li"):
+                inte = (li.find("p","interpret").contents[0],list())
+
+                example =  li.find_all("p","example")
+                for ex in example :
+                    out = ""
+                    for e in  ex.contents:
+                        out += unicode(e)
+                    out = out.replace("<b>","").replace("</b>","")
+                    outs = out.split("\n")
+                    inte[1].append((outs[0],outs[1]))
+
+                dd["interpret"].append(inte)
+            out_list.append(dd)
+        return out_list
+
+    def easy_fetch_li(self,class_name):
+        res = self.summary.find(attrs={"class":class_name})
+        if res :
+            li = list()
+            for data in res.find_all("li"):
+                li.append({"href":data.a["href"],"content":data.a.contents[0]})
+            return li
+        return None
+
+    def easy_fetch_a(self,class_name):
+        res = self.summary.find(attrs={"class":class_name})
+        if res :
+            li = list()
+            for data in res.find_all("a"):
+                li.append({"href":data["href"],"content":data.contents[0]})
+            return li
+        return None
+
+if __name__ == "__main__":
+    d = YahooDictAPI('attend')
+    print d.summary_desc
+    print d.summary_pron
+    print d.summary_related
+    print d.summary_synonym
+    print d.summary_antonym
+    print d.summary_variation
+    print d.get_defition()
+
+
